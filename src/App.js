dbb83752a70daf35784b1a33a29eab3fbfe2cacb
Aleksey Sergeev <avatar5555555@gmail.com>
2018-05-14 14:40:16 +0400

    Move playback methods and ref to App

--- src/App.js
+++ src/App.js
@@ -14,19 +14,53 @@ export class App extends Component {
   state = {
     track: null,
     index: null,
-    isSameTrack: false
+    isPlaying: false,
+    timerId: null,
+    currentTime: 0,
+    volumeLevel: 0,
+    isMuted: false
   }
 
   componentDidMount() {
     this.props.fetchItems()
+
+    // save volume level to show it on page
+    this.setState({ volumeLevel: this.audioRef.current.volume })
+
+    // When the file has finished playing to the end start next track
+    this.audioRef.current.addEventListener('ended', () => {
+      this.handleForward()
+    })
   }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (
+      prevState.track &&
+      prevState.track.trackId !== this.state.track.trackId
+    ) {
+      return true
+    }
+
+    return null
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // if current track is new - stop previous playing and start new track
+    if (snapshot) {
+      return this.setState({ isPlaying: false }, () => this.handlePlay())
+    }
+  }
+
+  audioRef = React.createRef()
 
   updateItem = newIndex => {
     const { items } = this.props
 
+    // if forwarding from last track - play the first track
     if (newIndex >= items.length)
       return this.setState({ track: items[0], index: 0 })
 
+    // if backward from first track - play the last track
     if (newIndex < 0)
       return this.setState({ track: items[items.length], index: items.length })
 
@@ -34,27 +68,117 @@ export class App extends Component {
   }
 
   handleSelect = (track, index) => {
-    const { track: currentTrack } = this.state
+    const { track: currentTrack, isPlaying } = this.state
     const isSameTrack = currentTrack && currentTrack.trackId === track.trackId
-    this.setState({ track, index, isSameTrack })
+
+    // if track the same track and it playing - pause
+    if (isSameTrack && isPlaying) {
+      return this.handlePause()
+    }
+
+    // otherwise set new track and play it
+    this.setState({ track, index }, () => this.handlePlay())
+  }
+
+  handlePlay = () => {
+    const { isPlaying, timerId, index } = this.state
+
+    // save ref's currentTime in state to render it's value
+    const setPlayTimer = () =>
+      setInterval(
+        () => this.setState({ currentTime: this.audioRef.current.currentTime }),
+        300
+      )
+
+    // check if index and track set
+    if (index == null) return
+
+    // emulate stop since there's no suitable method
+    if (isPlaying) {
+      this.audioRef.current.pause()
+      this.audioRef.current.currentTime = 0
+      clearInterval(timerId)
+      return this.setState({ isPlaying: false, currentTime: 0 })
+    }
+
+    this.audioRef.current.play()
+    this.setState({ isPlaying: true, timerId: setPlayTimer() })
+  }
+
+  handlePause = () => {
+    const { timerId } = this.state
+
+    // pause playing and reset timer
+    this.audioRef.current.pause()
+    clearInterval(timerId)
+    this.setState({ isPlaying: false })
+  }
+
+  handleForward = () => {
+    const { index } = this.state
+
+    // use implicit coercion since it checks if index is null or undefined
+    const newIndex = index == null ? 0 : index + 1
+    this.updateItem(newIndex)
+  }
+
+  handleBackward = () => {
+    const { items } = this.props
+    const { index } = this.state
+    const lastIndex = items.length - 1
+
+    const newIndex = !Boolean(index) ? lastIndex : index - 1
+
+    this.updateItem(newIndex)
+  }
+
+  handleVolume = volume => {
+    if (typeof volume === 'number' && volume !== this.audioRef.current.volume) {
+      this.audioRef.current.volume = volume
+      this.setState({ volumeLevel: volume })
+    }
+  }
+
+  handleMute = () => {
+    this.audioRef.current.muted = !this.audioRef.current.muted
+    this.setState({ isMuted: this.audioRef.current.muted })
+  }
+
+  handleSeeked = time => {
+    this.audioRef.current.currentTime = time
+    this.setState({ currentTime: time })
   }
 
   render() {
     const { items, isLoading, fetchItems } = this.props
-    const { track, index, isSameTrack } = this.state
-    const lastIndex = items.length - 1
+    const { track, volumeLevel, currentTime, isMuted, isPlaying } = this.state
+
+    const currentUrl = track ? track.previewUrl : ''
+    const duration =
+      this.audioRef && this.audioRef.current && this.audioRef.current.duration
+        ? this.audioRef.current.duration
+        : 0
 
     return (
       <Container>
         <Row>
           <Col xs={12} lg={{ size: 6, offset: 3 }}>
+            <audio src={currentUrl} ref={this.audioRef} />
             <PlaybackCard
               {...track}
+              duration={duration}
+              volumeLevel={volumeLevel}
+              currentTime={currentTime}
+              isMuted={isMuted}
+              isPlaying={isPlaying}
+              mute={this.handleMute}
+              play={this.handlePlay}
+              pause={this.handlePause}
+              forward={this.handleForward}
+              backward={this.handleBackward}
+              seeked={this.handleSeeked}
+              volume={this.handleVolume}
               onSubmit={fetchItems}
-              index={index}
-              updateItem={this.updateItem}
-              lastIndex={lastIndex}
-              isSameTrack={isSameTrack}
             />
             <PlayList
               items={items}
--- src/containers/PlaybackCard/index.js
+++ src/containers/PlaybackCard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -45,175 +45,88 @@ const InfoRoot = styled.div`
   align-items: center;
 `
 
-class PlaybackCard extends Component {
-  state = {
-    volumeLevel: 0,
-    isMuted: false,
-    isPlaying: false,
-    currentTime: 0,
-    timerId: null
-  }
+const PlaybackCard = ({
+  artworkUrl100,
+  artistName,
+  trackName,
+  duration,
+  volumeLevel,
+  currentTime,
+  isMuted,
+  isPlaying,
+  mute,
+  play,
+  pause,
+  forward,
+  backward,
+  seeked,
+  volume,
+  onSubmit
+}) => (
+  <Root>
+    <InfoRoot>
+      {artworkUrl100 ? (
+        <img src={artworkUrl100} alt="album" />
+      ) : (
+        <Icon name="cd" small={false} />
+      )}
 
-  componentDidMount() {
-    this.setState({ volumeLevel: this.audioRef.volume })
-  }
+      <Text>{artistName && `${artistName} - ${trackName}`}</Text>
+    </InfoRoot>
 
-  getSnapshotBeforeUpdate(prevProps) {
-    if (prevProps.trackId !== this.props.trackId) {
-      return true
-    }
+    <ControlsRoot>
+      <VolumeControl
+        volume={volumeLevel}
+        volumeChange={volume}
+        isMuted={isMuted}
+        mute={mute}
+      />
+      <PlaybackControl
+        play={play}
+        pause={pause}
+        forward={forward}
+        backward={backward}
+        isPlaying={isPlaying}
+      />
+    </ControlsRoot>
 
-    return null
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (snapshot) {
-      return this.setState({ isPlaying: false }, () => this.handlePlay())
-    }
-  }
-
-  audioRef
-
-  handlePlay = () => {
-    const { isPlaying, timerId } = this.state
-    const { index } = this.props
-    const setPlayTimer = () =>
-      setInterval(
-        () => this.setState({ currentTime: this.audioRef.currentTime }),
-        300
-      )
-
-    if (index == null) return
-
-    // emulate stop since there's no suitable method
-    if (isPlaying) {
-      this.audioRef.pause()
-      this.audioRef.currentTime = 0
-      clearInterval(timerId)
-      return this.setState({ isPlaying: false, currentTime: 0 })
-    }
-
-    this.audioRef.play()
-    this.setState({ isPlaying: true, timerId: setPlayTimer() })
-  }
-
-  handlePause = () => {
-    const { timerId } = this.state
-
-    this.audioRef.pause()
-    clearInterval(timerId)
-    this.setState({ isPlaying: false })
-  }
-
-  handleForward = () => {
-    const { index, updateItem } = this.props
-    updateItem(index + 1)
-  }
-
-  handleForward = () => {
-    const { index, updateItem } = this.props
-
-    // use implicit coercion since it checks if index is null or undefined
-    const newIndex = index == null ? 0 : index + 1
-    updateItem(newIndex)
-  }
-
-  handleBackward = () => {
-    const { index, updateItem, lastIndex } = this.props
-
-    const newIndex = !Boolean(index) ? lastIndex : index - 1
-
-    updateItem(newIndex)
-  }
-
-  updateVolume = volume => {
-    if (typeof volume === 'number' && volume !== this.audioRef.volume) {
-      this.audioRef.volume = volume
-      this.setState({ volumeLevel: volume })
-    }
-  }
-
-  handleMute = () => {
-    this.audioRef.muted = !this.audioRef.muted
-    this.setState({ isMuted: this.audioRef.muted })
-  }
-
-  handleSeeked = time => {
-    this.audioRef.currentTime = time
-    this.setState({ currentTime: time })
-  }
-
-  render() {
-    const {
-      artworkUrl100,
-      previewUrl,
-      artistName,
-      trackName,
-      onSubmit
-    } = this.props
-
-    const { volumeLevel, isMuted, isPlaying, currentTime } = this.state
-
-    const duration =
-      this.audioRef && this.audioRef.duration ? this.audioRef.duration : 0
-
-    return (
-      <Root>
-        <audio src={previewUrl} ref={ref => (this.audioRef = ref)} />
-        <InfoRoot>
-          {artworkUrl100 ? (
-            <img src={artworkUrl100} alt="album" />
-          ) : (
-            <Icon name="cd" small={false} />
-          )}
-          <Text>{artistName && `${artistName} - ${trackName}`}</Text>
-        </InfoRoot>
-
-        <ControlsRoot>
-          <VolumeControl
-            volume={volumeLevel}
-            volumeChange={this.updateVolume}
-            isMuted={isMuted}
-            mute={this.handleMute}
-          />
-          <PlaybackControl
-            play={this.handlePlay}
-            pause={this.handlePause}
-            forward={this.handleForward}
-            backward={this.handleBackward}
-            isPlaying={isPlaying}
-          />
-        </ControlsRoot>
-
-        <Playback
-          timeSeeked={this.handleSeeked}
-          duration={duration}
-          currentTime={currentTime}
-        />
-        <SearchBar onSubmit={onSubmit} />
-      </Root>
-    )
-  }
-}
+    <Playback
+      timeSeeked={seeked}
+      duration={duration}
+      currentTime={currentTime}
+    />
+    <SearchBar onSubmit={onSubmit} />
+  </Root>
+)
 
 PlaybackCard.propTypes = {
   artworkUrl100: PropTypes.string,
-  previewUrl: PropTypes.string,
   artistName: PropTypes.string,
   trackName: PropTypes.string,
   onSubmit: PropTypes.func.isRequired,
-  index: PropTypes.number,
-  updateItem: PropTypes.func.isRequired,
-  lastIndex: PropTypes.number
+  duration: PropTypes.number,
+  volumeLevel: PropTypes.number,
+  isMuted: PropTypes.bool,
+  isPlaying: PropTypes.bool,
+  currentTime: PropTypes.number,
+  mute: PropTypes.func.isRequired,
+  play: PropTypes.func.isRequired,
+  pause: PropTypes.func.isRequired,
+  forward: PropTypes.func.isRequired,
+  backward: PropTypes.func.isRequired,
+  seeked: PropTypes.func.isRequired,
+  volume: PropTypes.func.isRequired
 }
 
 PlaybackCard.defaultProps = {
   artworkUrl100: '',
-  previewUrl: '',
   artistName: '',
   trackName: '',
-  index: null,
-  lastIndex: 0
+  duration: 0,
+  volumeLevel: 0,
+  currentTime: 0,
+  isMuted: false,
+  isPlaying: false
 }
 
 export default PlaybackCard
